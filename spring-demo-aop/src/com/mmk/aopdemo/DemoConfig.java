16dd1a7f3d0547b12f9511c7cdeabcf66c7ee527
jon <manojam79@gmail.com>
2019-03-16 16:10:53 +1100

    @BeforeAdvice AspectJ

---
+++ spring-demo-aop/src/com/mmk/aopdemo/DemoConfig.java
@@ -0,0 +1,12 @@
+package com.mmk.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.mmk")
+public class DemoConfig {
+	
+}
