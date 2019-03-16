4dcbf08b5616c77d2d836511479a02601773a0bf
jon <manojam79@gmail.com>
2019-03-16 22:15:49 +1100

    Pointcut expressions

--- spring-demo-aop/src/com/mmk/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/mmk/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,16 +1,21 @@
 package com.mmk.aopdemo.aspect;
 
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
-	// this is where we add all our related advices for logging
+	// this is where we add all our related advices for logging	
 
 	// @AddBefore advice
-	@Before("execution(public void addAccount())")
+//	@Before("execution(public void addAccount())")
+//	@After("execution(public void addAccount())")
+	@Around("execution(public void addAccount())")
+//	@AfterReturning("execution(public void addAccount())")
+//	@("execution(public void addAccount())")
 	public void beforeAddAccountAdvice() {
 		System.out.println("===== Executing Before Advice =====");
 	}
